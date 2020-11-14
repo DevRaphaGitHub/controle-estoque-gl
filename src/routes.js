bde4a2d2aa6d62894cc4867bce66ae744fba2931
Raphael Cunha <raphaelcdutra93@gmail.com>
2020-11-14 17:33:16 -0300

    Alterada porta do backend

--- src/routes.js
+++ src/routes.js
@@ -4,29 +4,23 @@ const mysql = require('mysql');
 
 // Conexão com o banco de dados
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'e3n1d7a9',
-  database: 'controle_estoque_gl'
+    host: 'localhost',
+    user: 'root',
+    password: 'e3n1d7a9',
+    database: 'controle_estoque_gl'
 });
 
 // Inicializando as rotas pelo Express
 const app = express();
 
 // Criando uma rota GET que retorna os dados da tabela veículos.
-app.get('/veiculos', function (req, res) {
-    // Conectando ao banco.
-    connection.getConnection(function (err, connection) {
-    
-    // Executando a query MySQL (selecionar todos os dados da tabela veículos).
-    connection.query('SELECT * FROM veiculos', function (error, results, fields) {
-      // Caso ocorra algum erro, não irá executar corretamente.if (error) throw error;
-      
-      // Pegando a 'resposta' do servidor pra nossa requisição. Ou seja, aqui ele vai mandar nossos dados.
-      res.send(results)
+app.get('/veiculos', function(req, res) {
+    connection.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM veiculos', function(error, results, fields) {
+            res.send(results)
+        });
     });
-  });
 });
 
 // Iniciando o servidor.
-app.listen(3000);
+app.listen(3333);
