065b71ca20da6261368acfeddbd7eadf8ded79c1
Raphael Cunha <raphaelcdutra93@gmail.com>
2020-11-17 10:56:21 -0300

    Conexão do banco de dados

--- src/backend/database/connection.js
+++ src/backend/database/connection.js
@@ -3,10 +3,10 @@ import knex from 'knex';
 const connection = knex({
     client: 'mysql',
     connection: {
-      host: 'localhost',
-      user: 'root',
-      password: 'e3n1d7a9',
-      database: 'controle_estoque_gl'
+      host: 'us-cdbr-east-02.cleardb.com',
+      user: 'b1de9de9fe8c15',
+      password: '249a23fa4ce1b36',
+      database: 'heroku_ccf3d9670930e6b'
     },
     useNullAsDefault: true,
 });
