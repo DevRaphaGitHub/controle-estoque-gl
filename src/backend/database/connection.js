d328d2efd3757a255e9a2084b1a1f686c760bddb
Raphael Cunha <raphaelcdutra93@gmail.com>
2020-11-23 11:01:04 -0300

    ...

--- src/backend/database/connection.js
+++ src/backend/database/connection.js
@@ -1,5 +1,9 @@
 import knex from 'knex';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const connection = knex({
   client: 'sqlite3',
