edb1901ba414965b57e4dd8c0889b33fb2f70a64
Raphael Cunha <raphaelcdutra93@gmail.com>
2020-11-23 11:13:51 -0300

    Conexão MySQL

--- src/backend/database/connection.js
+++ src/backend/database/connection.js
@@ -1,16 +1,30 @@
 import knex from 'knex';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const connection = knex({
-  client: 'sqlite3',
+  client: 'mysql',
   connection: {
-    __filename: path.resolve(__dirname, 'database.sqlite')
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b1990d5a226459',
+    password: '25d2940f',
+    database: 'heroku_16e5bf51f1e35b2'
   },
-  useNullAsDefault: true
-});
+})
 
 export default connection;
+
+// import knex from 'knex';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const connection = knex({
+//   client: 'sqlite3',
+//   connection: {
+//     __filename: path.resolve(__dirname, 'database.sqlite')
+//   },
+//   useNullAsDefault: true
+// });
+
+// export default connection;
