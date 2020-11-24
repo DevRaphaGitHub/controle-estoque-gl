6342cb432b40c7897846a9a4f524ba880b4eeaae
Raphael Cunha <raphaelcdutra93@gmail.com>
2020-11-23 21:56:14 -0300

    ...

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,17 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes.js';
-import path from 'path';
 
 const app = express();
 
-const PORT = process.env.PORT || 3333;
-
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.static(buildPath));
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor iniciado na porta ${PORT}`);
-});
+app.listen(3333);
